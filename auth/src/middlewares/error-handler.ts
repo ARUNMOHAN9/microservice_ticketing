de175c8da3ddb0a3e6d4c327217efe0cf0befdca
Arun Mohan <arunmh7@gmail.com>
2020-11-05 23:40:57 +0530

    added not found error and code refactor

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { ErrorRequestHandler } from "express";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
-import { RequestValidationError } from "../errors/request-validation-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler: ErrorRequestHandler = (
   err: Error,
@@ -8,16 +7,9 @@ export const errorHandler: ErrorRequestHandler = (
   res,
   next
 ) => {
-  if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map((error) => ({
-      message: error.msg,
-      field: error.param,
-    }));
-
-    return res.status(400).send({ errors: formattedErrors });
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({ errors: [{ message: err.reason }] });
-  }
+  res.status(500).send({ errors: [{ message: "Something went wrong" }] });
 };
