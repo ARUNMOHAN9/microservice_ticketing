c2a6f7b4cdfec826753def3384a015f3d3a3bcb6
Arun Mohan <arunmh7@gmail.com>
2020-11-14 15:08:58 +0530

    added additional test cases

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo = new MongoMemoryServer();
 
@@ -28,3 +37,20 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const reponse = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: email,
+      password: password,
+    })
+    .expect(201);
+
+  const cookie = reponse.get("Set-Cookie");
+
+  return cookie;
+};
