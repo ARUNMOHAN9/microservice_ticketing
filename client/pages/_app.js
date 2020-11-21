bd2113b649baf1ec65276983cd08527c25f78629
Arun Mohan <arunmh7@gmail.com>
2020-11-21 13:22:32 +0530

    called getInitial props of landing page from appComponent

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-const App = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentuser }) => {
+
+    return (
+        <div>
+            <h1>{currentuser.email}</h1>
+            <Component {...pageProps} />
+        </div>
+    );
 };
 
-export default App;
+AppComponent.getInitialProps = async (appContext) => {
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get('/api/users/currentuser');
+
+    let pageProps = {};
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+    }
+
+    return {
+        pageProps,
+        ...data
+    };
+}
+
+export default AppComponent;
