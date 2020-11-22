6b9c1af7a0e77685d2452b4533fa3f130cb45cf4
Arun Mohan <arunmh7@gmail.com>
2020-11-22 12:30:44 +0530

    added test template

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,9 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 
-import { errorHandler,NotFoundError  } from "@ticketsam/common";
+import { errorHandler, NotFoundError } from "@ticketsam/common";
+
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set("trust proxy", true);
@@ -12,10 +14,11 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    secure: process.env.NODE_ENV !== "test",
   })
 );
 
+app.use(createTicketRouter);
 app.all("*", async () => {
   throw new NotFoundError();
 });
